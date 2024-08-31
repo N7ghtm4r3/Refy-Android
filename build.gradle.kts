0468240558f75cedeaa9ef18b256df0c5b3dbb81
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-08-31 10:53:10 +0200

    Implemented Dokka!

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.dokka) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.dokka.base)
+    }
 }
